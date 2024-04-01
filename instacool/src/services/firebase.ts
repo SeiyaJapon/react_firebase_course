ba73d4bcd55c1e845f8169d2c68897185a42e5ee
Fran <francisco.perez@bolttech.io>
2024-04-01 14:33:00 +0200

    first commit

--- instacool/src/services/firebase.ts
+++ instacool/src/services/firebase.ts
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCQU9S0PDqmuJopRkW34HK96Z3-oxfJ7E0",
-    appId: "1:385926902412:web:5a8fd4b51cfdb4f3e501a9",
-    authDomain: "instacool-ce5aa.firebaseapp.com",
-    databaseURL: "https://instacool-ce5aa.firebaseio.com",
-    measurementId: "G-5SJQRCDCB4",
-    messagingSenderId: "385926902412",
-    projectId: "instacool-ce5aa",
-    storageBucket: "instacool-ce5aa.appspot.com",
+    apiKey: "*****",
+    appId: "****",
+    authDomain: "*****.firebaseapp.com",
+    databaseURL: "https://*****.firebaseio.com",
+    measurementId: "*********",
+    messagingSenderId: "****",
+    projectId: "****",
+    storageBucket: "*****.appspot.com",
 };
 
 firebase.initializeApp(config);
